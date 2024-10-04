c824550b0355644ae0644a4ce9f858f64da1eb27
bolsonarocapitalista <142528137+bolsonarocapitalista@users.noreply.github.com>
2024-10-04 11:22:01 -0300

    dd

---
+++ main.js
@@ -0,0 +1,16 @@
+// function areaQuadrado (lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(2,2));
+// let média=function(num1,num2,num3){
+//     return (num1,num2,num3)/num3;
+// }
+// console.log(média(1,2,3));
+
+
+// let num1= parseInt(prompt(8+8+0/3));
+// let num2= parseInt(prompt('Qual n?'));
+// let num3= parseInt(prompt('Qual n?'));
+// document.write(8+8+0/3);
+
+let estados = [RS,SC,PR,SP,MS,MT,RJ,ES,MG,GO,DF,TO,BA,RO,SE,AL,PE,PB,RN,CE,PI,MA,PA,AP,RR,AM,AC]
